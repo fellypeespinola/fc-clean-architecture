8135d693aeda135be48ac8be9442bba4654122e8
Fellype Espínola <fellype@findme.id>
2023-07-31 21:35:37 -0300

    refactore: add validation with notification pattern

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -2,7 +2,6 @@ import ProductInterface from "./product.interface";
 import Entity from "../../@shared/entity/entity.abstract";
 import Notification from "../../@shared/notification/notification";
 import NotificationError from "../../@shared/notification/notification.error";
-import ProductValidatorFactory from "../factory/product.validator.factory";
 
 export default class Product extends Entity implements ProductInterface {
   private _name: string;
@@ -39,7 +38,25 @@ export default class Product extends Entity implements ProductInterface {
     this.validate();
   }
 
-  validate() {
-    ProductValidatorFactory.create().validate(this);
+  validate(): boolean {
+    if (this._id.length === 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Id is required",
+      });
+    }
+    if (this._name.length === 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Name is required",
+      });
+    }
+    if (this._price < 0) {
+      this.notification.addError({
+        context: "product",
+        message: "Price must be greater than zero",
+      });
+    }
+    return true;
   }
 }
--- src/domain/product/factory/product.validator.factory.ts
+++
@@ -1,9 +0,0 @@
-import Product from "../entity/product";
-import ProductYupValidator from "../validator/product.yup.validator";
-import ValidatorInterface from "../../@shared/validator/validator.interface";
-
-export default class ProductValidatorFactory {
-  static create(): ValidatorInterface<Product> {
-    return new ProductYupValidator();
-  }
-}
